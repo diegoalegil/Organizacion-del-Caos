ffbe81d8acc6b01a3a1a16ae232e51d22962f01c
diegoalegil <diegogildam@gmail.com>
2026-02-24 20:10:34 +0000

    punto 2

--- agente-del-caos/limpiador.sh
+++ agente-del-caos/limpiador.sh
@@ -17,17 +17,56 @@ mkdir -p IMGS
 mkdir -p DOCS
 mkdir -p TXTS
 mkdir -p PDFS
-mkdir -p VACIOS    
+mkdir -p VACIOS
 
-mv *.jpg *.png *.gif IMGS/ 2>/dev/null
-mv *.docx *.odt DOCS/ 2>/dev/null
-mv *.txt TXTS/ 2>/dev/null
-mv *.pdf PDFS/ 2>/dev/null
+imagenes=0
+documentos=0
+textos=0
+pdfs=0
+vacios=0    
 
-for archivo in *; do
-    if [ -f "$archivo" ] && [ ! -s "$archivo" ]; then
-        mv "$archivo" VACIOS
+or archivo in *.jpg *.png *.gif; do
+    if [ -f "$archivo" ]; then
+        mv "$archivo" IMGS/
+        imagenes=$((imagenes+1))
     fi
 done
 
+for archivo in *.docx *.odt; do
+    if [ -f "$archivo" ]; then
+        mv "$archivo" DOCS/
+        documentos=$((documentos+1))
+    fi
+done
+
+for archivo in *.txt; do
+    if [ -f "$archivo" ]; then
+        mv "$archivo" TXTS/
+        textos=$((textos+1))
+    fi
+done
+
+for archivo in *.pdf; do
+    if [ -f "$archivo" ]; then
+        mv "$archivo" PDFS/
+        pdfs=$((pdfs+1))
+    fi
+done
+
+# Archivos vacíos
+for archivo in *; do
+    if [ -f "$archivo" ] && [ ! -s "$archivo" ]; then
+        mv "$archivo" VACIOS/
+        vacios=$((vacios+1))
+    fi
+done
+
+echo
+echo "------ INFORME ------"
+echo "Se han movido $imagenes imágenes."
+echo "Se han movido $documentos documentos."
+echo "Se han movido $textos archivos de texto."
+echo "Se han movido $pdfs PDFs."
+echo "Se encontraron $vacios archivos vacíos."
+echo "---------------------"
 echo "Limpieza terminada."

da74ca61956f279af6651fd79a8cce58fe522ddf
diegoalegil <diegogildam@gmail.com>
2026-02-24 19:58:30 +0000

    Implemente el punto 1 limpiador.sh

--- agente-del-caos/limpiador.sh
+++ agente-del-caos/limpiador.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+mkdir -p IMGS DOCS TXTS PDFS VACIOS
+
+mv *.jpg *.png *.gif IMGS/ 2>/dev/null
+mv *.docx *.odt DOCS/ 2>/dev/null
+mv *.txt TXTS/ 2>/dev/null
+mv *.pdf PDFS/ 2>/dev/null
+
 
 
